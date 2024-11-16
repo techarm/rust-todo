964b160d3ec70cd884c05aac9c1f22a70fc500be
techarm <techarmdev@gmail.com>
2024-11-16 15:07:36 +0900

    APIのハンドラーテストを追加

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use std::env;
-use axum::{Json, Router};
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::routing::{get, post};
+use axum::{Json, Router};
 use serde::{Deserialize, Serialize};
+use std::env;
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
@@ -12,18 +13,26 @@ async fn main() {
     env::set_var("RUST_LOG", log_level);
     tracing_subscriber::fmt::init();
 
-    let app = Router::new()
-        .route("/", get(root))
-        .route("/users", post(create_user));
+    let app = create_app();
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    tracing::debug!("listening on {:?}", listener);
-    axum::serve(listener, app).await.unwrap();
+    // let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+fn create_app() -> Router {
+    Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user))
 }
 
 async fn root() -> &'static str {
-    "Hello, world!"
+    "Hello, World!"
 }
 
 async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
@@ -34,13 +43,52 @@ async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
     (StatusCode::CREATED, Json(user))
 }
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize, Debug, PartialOrd, PartialEq)]
 struct CreateUser {
     username: String,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize, Debug, PartialOrd, PartialEq)]
 struct User {
     id: u64,
     username: String,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use axum::body::Body;
+    use axum::http::{Method, Request};
+    use hyper::header;
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn should_return_hello_world() {
+        let req = Request::builder().uri("/").body(Body::empty()).unwrap();
+        let res = create_app().oneshot(req).await.unwrap();
+        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let body: String = String::from_utf8(bytes.to_vec()).unwrap();
+        assert_eq!(body, "Hello, World!");
+    }
+
+    #[tokio::test]
+    async fn should_return_user_data() {
+        let req = Request::builder()
+            .uri("/users")
+            .method(Method::POST)
+            .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+            .body(Body::from(r#"{ "username": "techarm" }"#))
+            .unwrap();
+        let res = create_app().oneshot(req).await.unwrap();
+        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let body: String = String::from_utf8(bytes.to_vec()).unwrap();
+        let user: User = serde_json::from_str(&body).expect("cannot convert User instance.");
+        assert_eq!(
+            user,
+            User {
+                id: 1337,
+                username: "techarm".to_string(),
+            }
+        )
+    }
 }
