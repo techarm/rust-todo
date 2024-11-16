27a201f95622430c23bce7bf73e53435eb3e5992
techarm <techarmdev@gmail.com>
2024-11-16 15:29:11 +0900

    Todo関連処理のリポジトリを追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::routing::{get, post};
@@ -5,6 +6,88 @@ use axum::{Json, Router};
 use serde::{Deserialize, Serialize};
 use std::env;
 use std::net::SocketAddr;
+use std::sync::{Arc, RwLock};
+use axum::extract::Extension;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+enum RepositoryError {
+    #[error("NotFound, id is {0}")]
+    NotFound(i32),
+}
+
+pub trait TodoRepository: Clone + Send + Sync + 'static {
+    fn create(&self, payload: CreateTodo) -> Todo;
+    fn find(&self, id: i32) -> Option<Todo>;
+    fn all(&self) -> Vec<Todo>;
+    fn update(&self, id: i32, payload: UpdateTodo) -> anyhow::Result<Todo>;
+    fn delete(&self, id: i32) ->  anyhow::Result<()>;
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialOrd, PartialEq)]
+pub struct Todo {
+    id: i32,
+    text: String,
+    completed: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialOrd, PartialEq)]
+pub struct CreateTodo {
+    text: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialOrd, PartialEq)]
+pub struct UpdateTodo {
+    text: Option<String>,
+    completed: Option<bool>,
+}
+
+impl Todo {
+    pub fn new(id: i32, text: String) -> Self {
+        Self {
+            id,
+            text,
+            completed: false
+        }
+    }
+}
+
+type TodoDatas = HashMap<i32, Todo>;
+
+#[derive(Debug, Clone)]
+pub struct TodoRepositoryForMemory {
+    store: Arc<RwLock<TodoDatas>>,
+}
+
+impl TodoRepositoryForMemory {
+    pub fn new() -> Self {
+        TodoRepositoryForMemory {
+            store: Arc::default()
+        }
+    }
+}
+
+impl TodoRepository for TodoRepositoryForMemory {
+    fn create(&self, payload: CreateTodo) -> Todo {
+        todo!()
+    }
+
+    fn find(&self, id: i32) -> Option<Todo> {
+        todo!()
+    }
+
+    fn all(&self) -> Vec<Todo> {
+        todo!()
+    }
+
+    fn update(&self, id: i32, payload: UpdateTodo) -> anyhow::Result<Todo> {
+        todo!()
+    }
+
+    fn delete(&self, id: i32) -> anyhow::Result<()> {
+        todo!()
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -13,7 +96,8 @@ async fn main() {
     env::set_var("RUST_LOG", log_level);
     tracing_subscriber::fmt::init();
 
-    let app = create_app();
+    let repository = TodoRepositoryForMemory::new();
+    let app = create_app(repository);
 
     // run our app with hyper, listening globally on port 3000
     // let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -25,10 +109,12 @@ async fn main() {
         .unwrap();
 }
 
-fn create_app() -> Router {
+fn create_app<T: TodoRepository>(repository: T) -> Router {
     Router::new()
         .route("/", get(root))
         .route("/users", post(create_user))
+        .route("/todos", post(create_todo::<T>))
+        .layer(Extension(Arc::new(repository)))
 }
 
 async fn root() -> &'static str {
@@ -41,6 +127,11 @@ async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
         username: payload.username,
     };
     (StatusCode::CREATED, Json(user))
+}
+
+async fn create_todo<T: TodoRepository>(Json(payload): Json<CreateTodo>, Extension(repository): Extension<Arc<T>>) -> impl IntoResponse {
+    let todo = repository.create(payload);
+    (StatusCode::CREATED, Json(todo))
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialOrd, PartialEq)]
@@ -64,8 +155,9 @@ mod test {
 
     #[tokio::test]
     async fn should_return_hello_world() {
+        let repository = TodoRepositoryForMemory::new();
         let req = Request::builder().uri("/").body(Body::empty()).unwrap();
-        let res = create_app().oneshot(req).await.unwrap();
+        let res = create_app(repository).oneshot(req).await.unwrap();
         let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
         let body: String = String::from_utf8(bytes.to_vec()).unwrap();
         assert_eq!(body, "Hello, World!");
@@ -79,7 +171,8 @@ mod test {
             .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
             .body(Body::from(r#"{ "username": "techarm" }"#))
             .unwrap();
-        let res = create_app().oneshot(req).await.unwrap();
+        let repository = TodoRepositoryForMemory::new();
+        let res = create_app(repository).oneshot(req).await.unwrap();
         let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
         let body: String = String::from_utf8(bytes.to_vec()).unwrap();
         let user: User = serde_json::from_str(&body).expect("cannot convert User instance.");
